6c8b366c8768be9709394c212a008596c0706235
Lucas Fagundes Nunes <lucasfnunes6@gmail.com>
2023-02-04 17:06:33 -0300

    Programa desenvolvido para aprendizado de consumo Api

---
+++ lib/pages/detalhes_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Detalhes extends StatefulWidget {
+  @override
+  _DetalhesState createState() => _DetalhesState();
+}
+
+class _DetalhesState extends State<Detalhes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalhes"),
+        
+      ),
+    );
+  }
+}
